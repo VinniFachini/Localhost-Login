82def8cc6f36f98b0d707978f28233f818d02e17
Vinicius Fachini <vinniebrasil@gmail.com>
2020-08-06 14:51:13 -0300

    New update, jQuery validation

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $('input[type=submit]').click(function (e) { 
+        e.preventDefault();
+        var usr = $('input[type=text]').val();
+        var psw = $('input[type=password]').val();
+        if(usr == 'admin' && psw == 'admin') {
+            swal({
+                title: "Login",
+                text: "Usuario e senha estão corretos :D",
+                icon: "success"
+            });
+        } else {
+            swal({
+                title: "Login",
+                text: "Usuario e senha estão incorretos :/",
+                icon: "error",
+                button: "Tentar Novamente"
+            });
+        }
+       
+    });
+})
